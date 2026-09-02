b1f31f171794e9fd83d25a4138f7c0459169af41
nokiapro <xuankensinger@gmail.com>
2026-09-02 19:06:40 +0700

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -13096,7 +13096,7 @@ const DEFAULT_FERTILIZERS = [
 ];
 
 
-const APP_VERSION = '1.9.103';
+const APP_VERSION = '1.9.104';
 
 const DEFAULT_SETTINGS = {
   plotCount: 12,
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Vườn Xanh PWA service worker */
-const CACHE_VERSION = 'vuon-xanh-v1.9.103';
+const CACHE_VERSION = 'vuon-xanh-v1.9.104';
 const PRECACHE = [
   './',
   './index.html',
