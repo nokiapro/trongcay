5b64a9c14d9811ea287a9e624983ea99c6558873
nokiapro <xuankensinger@gmail.com>
2026-09-25 10:20:32 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Vườn Xanh PWA service worker */
-const CACHE_VERSION = 'vuon-xanh-v1.9.232-offline-run';
+const CACHE_VERSION = 'vuon-xanh-v1.9.232';
 const PRECACHE = [
   './',
   './index.html',
